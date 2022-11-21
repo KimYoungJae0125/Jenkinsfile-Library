8724c0d27899e3218771431001ff0e51513e31bf
kyj <uni0125@naver.com>
2022-11-22 00:17:47 +0900

    [Fix] sendNotification

--- src/jenkins/messages.groovy
+++ src/jenkins/messages.groovy
@@ -14,11 +14,35 @@ def test(String stage) {
         "type": "divider"
       ],
       [
-              "type": "section",
-              "text": [
-                      "type": "mrkdwn",
-                      "text": "Build, ${env.JOB_NAME}"
-              ]
+          "type": "section",
+          "text": [
+                  "type": "mrkdwn",
+                  "text": "${env.JOB_NAME} - #${env.BUILD_NUMBER}\n${env.BUILD_URL}"
+          ]
+      ],
+      [
+          "type": "section",
+          "text": [
+                  "type": "mrkdwn",
+                  "text": "${env.GIT_URL}(${env.GIT_BRANCH})"
+          ]
+      ],
+      [
+          "type": "section",
+          "text": [
+                  "type": "mrkdwn",
+                  "text": "${GIT_COMMIT_AUTHOR}(${GIT_COMMIT_AUTHOR_EMAIL})"
+          ]
+      ],
+      [
+          "type": "section",
+          "text": [
+                  "type": "mrkdwn",
+                  "text": "```${GIT_COMMIT_MESSAGE}```"
+          ]
+      ],
+      [
+              "type": "divider"
       ]
   ]
 
