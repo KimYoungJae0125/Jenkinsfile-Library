3b48ff273bf65e442f84f4858489c052841097dc
kyj <uni0125@naver.com>
2022-11-22 00:42:02 +0900

    [Fix] sendNotification

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -5,7 +5,8 @@ import jenkins.messages
 def call(String stage) {
     def messagesFile = new messages()
 
-    def status = currentBuild.result.toLowerCase()
+    def status = currentBuild.result
+    status = status.subString(0, 1) + status.subString(1).toLowerCase()
 
     slackSend(color: "good", blocks: messagesFile.test(stage, status))
 
